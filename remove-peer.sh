040052c587a50223812b791fbce2c4e9e897f2e1
ericwebb <ewebb1@gmail.com>
2020-08-05 00:21:36 -0400

    Update remove-peer.sh

--- remove-peer.sh
+++ remove-peer.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+# add peer script to wireguard by Eric Webb 8-3-2020 original script
+clear
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+echo
+echo "Please provide the name of the user"
+read -p 'Name: ' namevar
+clear
+echo "removing client config for: $namevar"
 
-if [ $# -eq 0 ]
-then
-	echo "must have peer id as arg: remove-peer.sh asdf123="
+ckey=$(cat clients/$namevar/$namevar.pub)
+
+echo $ckey
+
+if [ -n "$ckey" ]; then
+sudo wg set wg0 peer $ckey remove
+sudo wg show
 else
-	sudo wg set wg0 peer $1 remove
-	sudo wg show
+echo "Please check name"
 fi
